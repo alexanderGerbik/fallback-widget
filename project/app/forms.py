fc8c77b5ecb972a3a83723332176a5c5eac9704a
Alexander Gerbik <alexander.gerbik@gmail.com>
2019-10-29 21:46:07 +0300

    Use custom widget

--- project/app/forms.py
+++ project/app/forms.py
@@ -2,11 +2,18 @@ from django import forms
 from django.forms import widgets
 
 from app.models import Foo
+from app.widgets import FallbackSelect
 
 
 class FooForm(forms.ModelForm):
-    value = forms.CharField(widget=widgets.TextInput())
-    another_value = forms.CharField(widget=widgets.TextInput())
+    value = forms.CharField(widget=FallbackSelect(choices=(
+        ('value1', 'Pre-filled 1'),
+        ('value2', 'Pre-filled 2'),
+    )))
+    another_value = forms.CharField(widget=FallbackSelect(choices=(
+        ('1', 'Some value 1'),
+        ('2', 'Some value 2'),
+    ), attrs={'type': 'number'}, fallback_label='Custom...'))
 
     class Meta:
         model = Foo
